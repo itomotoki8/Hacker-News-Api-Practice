c7d8998319ed6e0f805d84dda1441a4d762b85a6
ito motoki <yitengb90@gmail.com>
2023-02-18 11:34:16 +0900

    オブジェクトの集まりを配列にする方法が分かりません

---
+++ src/components/ApiTop.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+
+const ApiTop = () => {
+
+  const newsApi = `https://hacker-news.firebaseio.com/v0/newstories.json`;
+
+
+
+  const [news,setNews] = useState([]);
+
+  useEffect(() => {
+    fetch(newsApi)
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      setNews(data)
+    })
+  },[]);
+
+
+  const a = news.map( value => {
+    return `https://hacker-news.firebaseio.com/v0/item/${value}.json`
+  });
+  const k = a.slice(0,20)
+
+
+
+
+  const [api, setApi] = useState([]);
+
+  useEffect(()=> {
+    k.map(data => {
+    fetch(data)
+    .then(res=> {
+      return res.json();
+    })
+    .then(data2 => {
+    //  console.log(data2);
+     setApi(data2);
+    })
+  });
+  },[news]);
+
+  console.log(api);
+
+
+
+
+
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default ApiTop
